387db8270e9a8babd3ea2bece3795f97b5fb7877
Bookingfor open source <a.bottacin@ipertrade.com>
2017-09-06 15:12:19 +0200

    "fix url" dettagli merchant

--- pkg_bookingforconnector/packages/com_bookingforconnector/site/views/shared/merchant_small_details.php
+++ pkg_bookingforconnector/packages/com_bookingforconnector/site/views/shared/merchant_small_details.php
@@ -36,17 +36,19 @@ if (empty($merchant->AddressData)){
 //		}
 //	}
 
-if (!isset($uriMerchant)) {
-   $db   = JFactory::getDBO();
+//TODO: da controllare perchè con risorse a catalogo non ricava l'url correttamente
+//if (!isset($uriMerchant)) {
+	$db   = JFactory::getDBO();
 	$itemIdMerchant=0;
 	$uriMerchant  = 'index.php?option=com_bookingforconnector&view=merchantdetails';
 	$db->setQuery('SELECT id FROM #__menu WHERE link LIKE '. $db->Quote( $uriMerchant .'%' ) .' AND (language='. $db->Quote($language) .' OR language='.$db->Quote('*').') AND published = 1 LIMIT 1' );
 	$itemIdMerchant = intval($db->loadResult());
+	
 	$uriMerchant.='&merchantId=' . $merchant->MerchantId . ':' . BFCHelper::getSlug($merchant->Name);
 	if ($itemIdMerchant<>0){
 		$uriMerchant.='&Itemid='.$itemIdMerchant;
 	}
-}
+//}
 //$uriMerchant = $routeMerchant;
 
 //$route = $routeMerchant;
